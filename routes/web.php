1fd21477c98d548c1919cf3acafa87d23d282fc7
Dede Kurniawan <dedekurniawan.dk67@gmail.com>
2024-12-28 00:15:11 +0700

    add crontabs

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/employees', function () {
+    $data = \DB::table('employees')->paginate(10);
+    return $data;
+});
+
+Route::get('/salaries', function () {
+    $data = \DB::table('salaries')->paginate(10);
+    return $data;
+});
